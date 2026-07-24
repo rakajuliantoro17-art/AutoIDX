a04c9e15b5ba7043e6a9ea3f2c509ad401db8779
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-24 23:23:41 +0700

    Add constants for default pairs and base URL

---
+++ src/api/market/constants.ts
@@ -0,0 +1,16 @@
+export const DEFAULT_PAIRS = [
+
+    "btcidr",
+
+    "ethidr",
+
+    "solidr",
+
+    "xrpidr",
+
+    "dogeidr",
+
+];
+
+export const BASE_URL =
+    "https://indodax.com/api";
