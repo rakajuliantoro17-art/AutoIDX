87fc8839551dac1506c980fd66c9c68c924e297b
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-24 23:23:59 +0700

    Add MarketTicker interface for market data

---
+++ src/api/market/types.ts
@@ -0,0 +1,17 @@
+export interface MarketTicker {
+
+    pair: string;
+
+    price: number;
+
+    buy: number;
+
+    sell: number;
+
+    high: number;
+
+    low: number;
+
+    volume: number;
+
+}
