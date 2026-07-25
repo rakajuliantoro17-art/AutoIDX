b0e98fee5b06c1e683b89e07c855759558721646
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-25 21:03:26 +0700

    Add Indodax service exports and default exports
    Added service exports for Indodax including client, ticker, market, candles, orderbook, and trades.

--- src/services/indodax/index.ts
+++ src/services/indodax/index.ts
@@ -1,1 +1,23 @@
+/**
+==========================================================
+AURA Trade OS
+Indodax Service Index
+Version : 0.0.5 Alpha
+==========================================================
+*/
 
+export * from "./client";
+export * from "./ticker";
+export * from "./market";
+export * from "./candles";
+export * from "./orderbook";
+export * from "./trades";
+export * from "./parser";
+
+// Default exports
+export { default as indodaxClient } from "./client";
+export { default as indodaxTickerService } from "./ticker";
+export { default as indodaxMarketService } from "./market";
+export { default as indodaxOrderbookService } from "./orderbook";
+export { default as indodaxTradesService } from "./trades";
+export { default as indodaxCandlesService } from "./candles";
