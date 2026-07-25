1c21a9a664fe948d559682b1c29fc3321194f405
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-25 21:26:15 +0700

    Add trading service exports and documentation header

--- src/services/trading/index.ts
+++ src/services/trading/index.ts
@@ -1,1 +1,40 @@
+/**
+==========================================================
+AURA Trade OS
+Trading Service
+Version : 0.0.6 Alpha
+==========================================================
+*/
 
+/*
+|--------------------------------------------------------------------------
+| Core Engine
+|--------------------------------------------------------------------------
+*/
+
+export { default as TradingEngine } from "./engine";
+export { default as DecisionEngine } from "./decision";
+export { default as TradeExecutor } from "./executor";
+
+/*
+|--------------------------------------------------------------------------
+| Trading Services
+|--------------------------------------------------------------------------
+*/
+
+export { default as PaperTradingService } from "./paper";
+export { default as PortfolioService } from "./portfolio";
+export { default as PositionService } from "./position";
+export { default as TradingHistory } from "./history";
+export { default as RiskManager } from "./risk";
+export { default as TradingStrategy } from "./strategy";
+
+/*
+|--------------------------------------------------------------------------
+| Types
+|--------------------------------------------------------------------------
+*/
+
+export * from "./types";
+export * from "./decision";
+export * from "./executor";
