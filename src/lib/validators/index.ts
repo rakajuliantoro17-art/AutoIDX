ed23266a61eb3459795085ccb88821e503d4d680
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-25 21:14:37 +0700

    Add header comment to index.ts
    Added a header comment to the validators index file.

--- src/lib/validators/index.ts
+++ src/lib/validators/index.ts
@@ -1,1 +1,19 @@
+/**
+==========================================================
+AURA Trade OS
+Validators
+Version : 0.0.5 Alpha
+==========================================================
+*/
 
+export * from "./api";
+export * from "./config";
+export * from "./env";
+export * from "./number";
+export * from "./pair";
+export * from "./portfolio";
+export * from "./risk";
+export * from "./scanner";
+export * from "./strategy";
+export * from "./timeframe";
+export * from "./trade";
