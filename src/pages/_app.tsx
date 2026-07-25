05043e36e6d3043e26f6b586778b704fb7058ff0
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-25 22:40:18 +0700

    Wrap Component with PagesRouteGuard

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,11 +8,14 @@ Version : 0.0.2 Alpha
 import type { AppProps } from "next/app";
 import "@/app/globals.css";
 import { AuthProvider } from "@/services/auth/AuthContext";
+import PagesRouteGuard from "@/components/auth/PagesRouteGuard";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <PagesRouteGuard>
+        <Component {...pageProps} />
+      </PagesRouteGuard>
     </AuthProvider>
   );
 }
