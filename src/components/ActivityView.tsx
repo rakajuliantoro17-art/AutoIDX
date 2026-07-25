89c1a8d8ae38123ef2b580296d1479ed8f80631b
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-25 08:57:46 +0700

    Create ActivityView component to display activity logs

---
+++ src/components/ActivityView.tsx
@@ -0,0 +1,12 @@
+/**
+==========================================================
+AURA Trade OS
+Activity View (sub-component)
+Version : 0.0.1 Alpha
+==========================================================
+*/
+import ActivityLogs from "@/components/ActivityLogs";
+
+export default function ActivityView() {
+  return <ActivityLogs logs={[]} />;
+}
