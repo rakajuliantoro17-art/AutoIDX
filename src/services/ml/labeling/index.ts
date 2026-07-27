6d19adaf530ef8eea96f587578bd8077cda9a53e
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-27 16:04:54 +0700

    Add exports for ML Labeling Module components

--- src/services/ml/labeling/index.ts
+++ src/services/ml/labeling/index.ts
@@ -1,1 +1,85 @@
+/**
+==========================================================
+AURA Trade OS
+ML Labeling Module Gateway
+Version : 0.1.0 Alpha
+==========================================================
+*/
+
+
+/**
+ * Label Engine
+ */
+
+export {
+
+  default as labelingEngine,
+
+  LabelingEngine
+
+} from "./engine";
+
+
+
+/**
+ * Label Validator
+ */
+
+export {
+
+  default as labelValidator,
+
+  LabelValidator
+
+} from "./validator";
+
+
+
+/**
+ * Label Strategies
+ */
+
+export {
+
+  labelingStrategies,
+
+  ReturnStrategy,
+
+  TrendStrategy,
+
+  MeanReversionStrategy,
+
+  BreakoutStrategy,
+
+  TripleBarrierStrategy
+
+} from "./strategies";
+
+
+
+/**
+ * Label Rules
+ */
+
+export {
+
+  default as labelRuleManager,
+
+  LabelRuleManager
+
+} from "./rules";
+
+
+
+/**
+ * Outcome Analyzer
+ */
+
+export {
+
+  default as outcomeAnalyzer,
+
+  OutcomeAnalyzer
+
+} from "./outcome";
 
