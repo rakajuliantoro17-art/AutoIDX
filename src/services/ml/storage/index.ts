7f05a58f750aa56733133d6013f5a72dc282c04d
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-27 16:57:56 +0700

    Refactor ML storage index to export services and types

--- src/services/ml/storage/index.ts
+++ src/services/ml/storage/index.ts
@@ -1,1 +1,54 @@
+/**
+==========================================================
+AURA Trade OS
+ML Storage Gateway
+Version : 0.1.0 Alpha
+==========================================================
+*/
 
+/*
+==========================================================
+Archive Service
+==========================================================
+*/
+
+export {
+  default as archiveService,
+  MLArchiveService
+} from "./archive";
+
+export type {
+  ArchiveEntry,
+  ArchiveType
+} from "./archive";
+
+/*
+==========================================================
+Repository
+==========================================================
+*/
+
+export {
+  default as InMemoryRepository
+} from "./repository";
+
+export type {
+  IRepository,
+  RepositoryRecord
+} from "./repository";
+
+/*
+==========================================================
+Runtime Loader
+==========================================================
+*/
+
+export {
+  default as mlLoader,
+  MLLoader
+} from "./loader";
+
+export type {
+  LoaderResult,
+  LoaderStatus
+} from "./loader";
