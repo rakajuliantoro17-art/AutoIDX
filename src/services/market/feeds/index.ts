f5fd919b061119d9b8a3c97bd60f28a9712d54ee
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-27 22:22:58 +0700

    Add documentation and export feeds in index.ts
    Added documentation header and organized public feed exports.

--- src/services/market/feeds/index.ts
+++ src/services/market/feeds/index.ts
@@ -1,1 +1,17 @@
+/**
+==========================================================
+AURA Trade OS
+Market Feeds
+Version : 0.1.1 Alpha
+==========================================================
+Public Feed Exports
+==========================================================
+*/
 
+export * from "./tickerFeed";
+
+export * from "./tradeFeed";
+
+export * from "./orderBookFeed";
+
+export * from "./candleFeed";
