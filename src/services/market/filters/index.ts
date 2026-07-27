0b231abb6d0f67fca8ce53e86cc9d7e255c91705
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-27 22:26:00 +0700

    Add documentation and export market filters
    Added documentation header and organized public filter exports.

--- src/services/market/filters/index.ts
+++ src/services/market/filters/index.ts
@@ -1,1 +1,17 @@
+/**
+==========================================================
+AURA Trade OS
+Market Filters
+Version : 0.1.1 Alpha
+==========================================================
+Public Filter Exports
+==========================================================
+*/
 
+export * from "./volumeFilter";
+
+export * from "./volatilityFilter";
+
+export * from "./spreadFilter";
+
+export * from "./liquidityFilter";
