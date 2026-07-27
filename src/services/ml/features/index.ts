8794c2fc5d7c902434b66819d0b3428f69343c07
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-27 15:57:49 +0700

    Add feature encoder and types exports

--- src/services/ml/features/index.ts
+++ src/services/ml/features/index.ts
@@ -1,1 +1,34 @@
+/**
+==========================================================
+AURA Trade OS
+ML Feature Engineering Module
+Version : 0.1.0 Alpha
+==========================================================
+*/
 
+
+/**
+ * Feature Encoder
+ */
+
+export {
+
+  default as featureEncoder,
+
+  FeatureEncoder
+
+} from "./encoder";
+
+
+
+/**
+ * Feature Types
+ */
+
+export type {
+
+  EncodingMethod,
+
+  EncodedFeature
+
+} from "./encoder";
