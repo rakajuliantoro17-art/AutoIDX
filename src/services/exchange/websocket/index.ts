6f209ff07a85a9dbd080674e0709c38c5f4ee4b4
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-27 22:07:23 +0700

    Update index.ts

--- src/services/exchange/websocket/index.ts
+++ src/services/exchange/websocket/index.ts
@@ -1,1 +1,13 @@
+/**
+==========================================================
+AURA Trade OS
+Exchange WebSocket Module
+Version : 0.1.1 Alpha
+==========================================================
+*/
 
+export * from "./reconnect";
+
+export * from "./market";
+
+export * from "./private";
