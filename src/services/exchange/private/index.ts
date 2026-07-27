1784f6982d393598e37a8609985486d266aef354
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-27 21:58:42 +0700

    Update index.ts

--- src/services/exchange/private/index.ts
+++ src/services/exchange/private/index.ts
@@ -1,1 +1,17 @@
+/**
+==========================================================
+AURA Trade OS
+Private Exchange Services
+Version : 0.1.1 Alpha
+==========================================================
+*/
 
+export * from "./account";
+
+export * from "./auth";
+
+export * from "./balance";
+
+export * from "./orders";
+
+export * from "./trades";
