ec038119283b8acd418336144207a63307c03253
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-27 22:18:54 +0700

    Add documentation and export aggregators
    Added documentation header and public aggregator exports.

--- src/services/market/aggregators/index.ts
+++ src/services/market/aggregators/index.ts
@@ -1,1 +1,17 @@
+/**
+==========================================================
+AURA Trade OS
+Market Aggregators
+Version : 0.1.1 Alpha
+==========================================================
+Public Aggregator Exports
+==========================================================
+*/
 
+export * from "./candleAggregator";
+
+export * from "./volumeAggregator";
+
+export * from "./tradeAggregator";
+
+export * from "./orderBookAggregator";
