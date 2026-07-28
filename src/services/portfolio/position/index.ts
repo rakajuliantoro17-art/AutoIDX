10983537542d778611132d222909e5f5552de627
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-28 20:43:40 +0700

    Refactor portfolio position module exports

--- src/services/portfolio/position/index.ts
+++ src/services/portfolio/position/index.ts
@@ -1,1 +1,82 @@
+/**
+==========================================================
+AURA Trade OS
+Portfolio Position Module Entry
+Version : 0.1.0 Alpha
+==========================================================
+*/
 
+
+/**
+==========================================================
+Position Manager
+==========================================================
+*/
+
+export {
+
+    default as positionManager
+
+}
+
+from "./positionManager";
+
+
+
+export {
+
+    PositionManager
+
+}
+
+from "./positionManager";
+
+
+
+export type {
+
+    Position,
+
+    PositionSide,
+
+    PositionUpdate,
+
+}
+
+from "./positionManager";
+
+
+
+/**
+==========================================================
+Position Calculator
+==========================================================
+*/
+
+export {
+
+    default as positionCalculator
+
+}
+
+from "./positionCalculator";
+
+
+
+export {
+
+    PositionCalculator
+
+}
+
+from "./positionCalculator";
+
+
+
+export type {
+
+    PositionCalculation,
+
+}
+
+from "./positionCalculator";
