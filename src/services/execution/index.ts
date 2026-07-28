9174772ee9578e6c41da5bd3ad5171fb2db6da97
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-28 19:40:57 +0700

    Implement public exports in execution index file
    Added public exports for types, core components, and singletons in the execution layer.

--- src/services/execution/index.ts
+++ src/services/execution/index.ts
@@ -1,1 +1,52 @@
+/**
+==========================================================
+AURA Trade OS
+Execution Layer
+Version : 0.1.0 Alpha
+==========================================================
+Public Exports
+==========================================================
+*/
 
+/*
+==========================================================
+Types
+==========================================================
+*/
+
+export * from "./types";
+
+/*
+==========================================================
+Core
+==========================================================
+*/
+
+export * from "./executionEngine";
+export * from "./executionLogger";
+export * from "./manager";
+export * from "./registry";
+
+/*
+==========================================================
+Singletons
+==========================================================
+*/
+
+export {
+
+    default as executionEngine,
+
+} from "./executionEngine";
+
+export {
+
+    default as executionManager,
+
+} from "./manager";
+
+export {
+
+    default as executionLogger,
+
+} from "./executionLogger";
