8438850e19674d7c2666905e997ec00212fdc1a6
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-28 19:29:54 +0700

    Add public strategy exports to index.ts

--- src/services/strategy/index.ts
+++ src/services/strategy/index.ts
@@ -1,1 +1,49 @@
+/**
+==========================================================
+AURA Trade OS
+Strategy Engine
+Version : 0.1.1 Alpha
+==========================================================
+Public Strategy Exports
+==========================================================
+*/
 
+/*
+==========================================================
+Core
+==========================================================
+*/
+
+export * from "./types";
+export * from "./registry";
+export * from "./engine";
+
+/*
+==========================================================
+Signals
+==========================================================
+*/
+
+export * from "./signals/buySignal";
+export * from "./signals/sellSignal";
+export * from "./signals/neutralSignal";
+
+/*
+==========================================================
+Rules
+==========================================================
+*/
+
+export * from "./rules/trendRule";
+export * from "./rules/momentumRule";
+export * from "./rules/volatilityRule";
+export * from "./rules/volumeRule";
+
+/*
+==========================================================
+Scoring
+==========================================================
+*/
+
+export * from "./scoring/confidence";
+export * from "./scoring/scoreEngine";
