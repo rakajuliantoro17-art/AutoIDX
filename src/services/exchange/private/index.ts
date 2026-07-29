1c68d59a57f532baedecd852e1e0a282b7781195
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-29 16:49:01 +0700

    Update index.ts

--- src/services/exchange/private/index.ts
+++ src/services/exchange/private/index.ts
@@ -5,13 +5,8 @@ Private Exchange Services
 Version : 0.1.1 Alpha
 ==========================================================
 */
-
 export * from "./account";
-
 export * from "./auth";
-
 export * from "./balance";
-
 export * from "./orders";
-
 export * from "./trades";
