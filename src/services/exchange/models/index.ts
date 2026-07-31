e359b068f4058c4f9b9d83ef92fc3ce6f17283e2
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-31 09:02:21 +0700

    Create index.ts for exchange models
    Initial export of exchange models for the AURA Trade OS.

---
+++ src/services/exchange/models/index.ts
@@ -0,0 +1,16 @@
+/**
+==========================================================
+AURA Trade OS
+Exchange Models Gateway
+Version : 0.1.0 Alpha
+==========================================================
+*/
+
+export * from "./account";
+export * from "./balance";
+export * from "./candle";
+export * from "./order";
+export * from "./pair";
+export * from "./position";
+export * from "./ticker";
+export * from "./trade";
