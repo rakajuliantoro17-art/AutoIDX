11d35b0ff485765ad28d3c550c6381e9fbe66f27
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-07-31 09:10:47 +0700

    Add Exchange Errors Gateway index file

---
+++ src/services/exchange/errors/index.ts
@@ -0,0 +1,12 @@
+/**
+==========================================================
+AURA Trade OS
+Exchange Errors Gateway
+Version : 0.1.0 Alpha
+==========================================================
+*/
+
+export * from "./ExchangeError";
+export * from "./AuthenticationError";
+export * from "./NetworkError";
+export * from "./RateLimitError";
