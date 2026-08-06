a3a89765fbe3b7fb13fb4fd66cb5fb72d193aac1
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-08-06 18:23:43 +0700

    Add header documentation and export statements
    Added documentation header and organized exports for the Pipeline module.

--- src/services/pipeline/index.ts
+++ src/services/pipeline/index.ts
@@ -1,1 +1,38 @@
+/**
+==========================================================
+AURA Trade OS
+Pipeline Module
+Version : 0.3.0 Alpha
+==========================================================
+Public Pipeline API
+==========================================================
+*/
+
+
+
+
+/*
+==========================================================
+Core
+==========================================================
+*/
+
+export * from "./pipeline";
+export * from "./pipelineStage";
+export * from "./pipelineContext";
+export * from "./pipelineResult";
+
+
+
+
+/*
+==========================================================
+Execution
+==========================================================
+*/
+
+export * from "./pipelineBuilder";
+export * from "./pipelineExecutor";
+export * from "./pipelineRegistry";
+export * from "./pipelineManager";
 
