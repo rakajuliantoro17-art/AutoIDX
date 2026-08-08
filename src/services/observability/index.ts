bef715173e3b7cd4c256efceb92118eebeb27080
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-08-08 07:12:27 +0700

    Bump version to 0.3.1 Alpha and update exports
    Updated version number from 0.3.0 Alpha to 0.3.1 Alpha and commented out unimplemented exports.

--- src/services/observability/index.ts
+++ src/services/observability/index.ts
@@ -2,64 +2,43 @@
 ==========================================================
 AURA Trade OS
 Observability Module
-Version : 0.3.0 Alpha
+Version : 0.3.1 Alpha
 ==========================================================
 Public Observability API
 ==========================================================
 */
-
-
-
-
 
 /*
 ==========================================================
 Core
 ==========================================================
 */
-
 export * from "./correlation";
 export * from "./traceContext";
 export * from "./tracing";
 export * from "./span";
-
-
-
-
 
 /*
 ==========================================================
 Profiling
 ==========================================================
 */
-
 export * from "./profiler";
 export * from "./profilerReport";
-
-
-
-
 
 /*
 ==========================================================
 Export
 ==========================================================
 */
-
 export * from "./traceExporter";
-
-
-
-
 
 /*
 ==========================================================
 Telemetry
 ==========================================================
+TODO: logging.ts, metrics.ts, telemetry.ts belum
+diimplementasikan -- sengaja tidak di-export di sini
+supaya tidak pura-pura sudah ada.
+==========================================================
 */
-
-export * from "./logging";
-export * from "./metrics";
-export * from "./telemetry";
-
-
