3a5a0dc446c79f929b5dd3683555ec506cd00e5c
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-08-08 07:50:56 +0700

    Expand health module exports with additional checks
    Added public health checks exports for various system components.

--- src/services/health/index.ts
+++ src/services/health/index.ts
@@ -2,11 +2,21 @@
 ==========================================================
 AURA Trade OS
 Health Module
+Version : 0.2.0 Alpha
 ==========================================================
-Barrel Export
+Public Health API
 ==========================================================
 */
 
-export * from './healthManager';
-export * from './readiness';
-export * from './healthReport';
+export * from "./healthManager";
+export * from "./readiness";
+export * from "./healthReport";
+
+export * from "./checks/databaseHealth";
+export * from "./checks/systemHealth";
+export * from "./checks/schedulerHealth";
+export * from "./checks/cacheHealth";
+export * from "./checks/memoryHealth";
+export * from "./checks/networkHealth";
+export * from "./checks/exchangeHealth";
+export * from "./checks/firebaseHealth";
