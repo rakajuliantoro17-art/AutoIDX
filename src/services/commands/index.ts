d5f22488d774a8e125c8d948d54166265c9d2982
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-08-08 21:39:10 +0700

    Add central command system exports in index.ts

--- src/services/commands/index.ts
+++ src/services/commands/index.ts
@@ -1,1 +1,245 @@
+/**
+==========================================================
+AURA Trade OS
+Commands Service
+Version : 0.0.7 Alpha
+==========================================================
+Central Command System Barrel
+==========================================================
+*/
 
+
+/*
+==========================================================
+ Core Command
+==========================================================
+*/
+
+export {
+    AURACommand,
+    createCommandId,
+    resolveCommandCategory,
+    isAURACommand,
+} from "./command";
+
+export type {
+    CommandOptions,
+    SerializedCommand,
+} from "./command";
+
+
+/*
+==========================================================
+ Command Type
+==========================================================
+*/
+
+export {
+    CommandType,
+} from "./commandType";
+
+
+/*
+==========================================================
+ Command Category
+==========================================================
+*/
+
+export {
+    CommandCategory,
+} from "./commandCategory";
+
+
+/*
+==========================================================
+ Command Priority
+==========================================================
+*/
+
+export {
+    CommandPriority,
+} from "./commandPriority";
+
+
+/*
+==========================================================
+ Command Status
+==========================================================
+*/
+
+export {
+    CommandStatus,
+} from "./commandStatus";
+
+
+/*
+==========================================================
+ Command Metadata
+==========================================================
+*/
+
+export type {
+    CommandMetadata,
+} from "./commandMetadata";
+
+export {
+    createCommandMetadata,
+} from "./commandMetadata";
+
+
+/*
+==========================================================
+ Command Context
+==========================================================
+*/
+
+export type {
+    CommandContext,
+} from "./commandContext";
+
+export {
+    createCommandContext,
+} from "./commandContext";
+
+
+/*
+==========================================================
+ Command Payload
+==========================================================
+*/
+
+export type {
+    CommandPayload,
+} from "./commandPayload";
+
+export {
+    isCommandPayloadObject,
+} from "./commandPayload";
+
+
+/*
+==========================================================
+ Command Result
+==========================================================
+*/
+
+export type {
+    CommandResult,
+} from "./commandResult";
+
+export {
+    createCommandResult,
+    createFailedCommandResult,
+    createRejectedCommandResult,
+} from "./commandResult";
+
+
+/*
+==========================================================
+ Command Handler
+==========================================================
+*/
+
+export type {
+    CommandHandler,
+    CommandHandlerRegistration,
+} from "./commandHandler";
+
+export {
+    createCommandHandlerId,
+} from "./commandHandler";
+
+
+/*
+==========================================================
+ Command Registry
+==========================================================
+*/
+
+export {
+    CommandRegistry,
+} from "./commandRegistry";
+
+
+/*
+==========================================================
+ Command Queue
+==========================================================
+*/
+
+export {
+    CommandQueue,
+} from "./commandQueue";
+
+
+/*
+==========================================================
+ Command Middleware
+==========================================================
+*/
+
+export {
+    CommandMiddlewareChain,
+} from "./commandMiddleware";
+
+export type {
+    CommandMiddleware,
+} from "./commandMiddleware";
+
+
+/*
+==========================================================
+ Command Dispatcher
+==========================================================
+*/
+
+export {
+    CommandDispatcher,
+} from "./commandDispatcher";
+
+
+/*
+==========================================================
+ Command Normalizer
+==========================================================
+*/
+
+export {
+    normalizeCommand,
+    normalizeCommandType,
+    isCommandType,
+} from "./commandNormalizer";
+
+
+/*
+==========================================================
+ Command Serializer
+==========================================================
+*/
+
+export {
+    CommandSerializer,
+} from "./commandSerializer";
+
+
+/*
+==========================================================
+ Command Bus
+==========================================================
+*/
+
+export {
+    CommandBus,
+} from "./commandBus";
+
+
+/*
+==========================================================
+ Command Manager
+==========================================================
+*/
+
+export {
+    CommandManager,
+    commandManager,
+} from "./commandManager";
