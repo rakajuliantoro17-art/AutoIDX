f62fa93087650ebe2c74ebcad760fe7d37d30660
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-08-09 21:33:49 +0700

    Update busType.ts

--- src/services/bus/busType.ts
+++ src/services/bus/busType.ts
@@ -1,1 +1,15 @@
+/**
+==========================================================
+AURA Trade OS
+Bus Type
+Version : 0.0.7 Alpha
+==========================================================
+*/
 
+export enum BusType {
+    COMMAND = "COMMAND",
+    EVENT = "EVENT",
+    MESSAGE = "MESSAGE",
+    QUERY = "QUERY",
+    INTERNAL = "INTERNAL",
+}
