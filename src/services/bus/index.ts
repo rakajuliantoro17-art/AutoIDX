ca36cb260c2a5b750c19090c234eacd9bf9df724
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-08-09 21:39:43 +0700

    Refactor bus service exports in index.ts

--- src/services/bus/index.ts
+++ src/services/bus/index.ts
@@ -1,1 +1,254 @@
+/**
+==========================================================
+AURA Trade OS
+Bus Service
+Version : 0.0.7 Alpha
+==========================================================
+Application Bus & Message Dispatching
+==========================================================
+*/
 
+
+/*
+==========================================================
+Bus
+==========================================================
+*/
+
+export {
+    ApplicationBus,
+    applicationBus,
+} from "./bus";
+
+
+/*
+==========================================================
+Bus Type
+==========================================================
+*/
+
+export {
+    BusType,
+} from "./busType";
+
+
+/*
+==========================================================
+Bus Message
+==========================================================
+*/
+
+export {
+    createBusMessage,
+    createBusMessageId,
+} from "./busMessage";
+
+export type {
+    BusMessage,
+} from "./busMessage";
+
+
+/*
+==========================================================
+Bus Result
+==========================================================
+*/
+
+export {
+    createBusSuccess,
+    createBusFailure,
+} from "./busResult";
+
+export type {
+    BusResult,
+} from "./busResult";
+
+
+/*
+==========================================================
+Bus Error
+==========================================================
+*/
+
+export {
+    BusError,
+    BusErrorCode,
+} from "./busError";
+
+
+/*
+==========================================================
+Bus Context
+==========================================================
+*/
+
+export {
+    createBusContext,
+    createDispatchId,
+} from "./busContext";
+
+export type {
+    BusContext,
+} from "./busContext";
+
+
+/*
+==========================================================
+Bus Handler
+==========================================================
+*/
+
+export {
+    createBusHandler,
+} from "./busHandler";
+
+export type {
+    BusHandler,
+    BusHandlerDefinition,
+} from "./busHandler";
+
+
+/*
+==========================================================
+Bus Middleware
+==========================================================
+*/
+
+export {
+    BusMiddlewareChain,
+} from "./busMiddleware";
+
+export type {
+    BusMiddleware,
+} from "./busMiddleware";
+
+
+/*
+==========================================================
+Bus Registry
+==========================================================
+*/
+
+export {
+    BusRegistry,
+} from "./busRegistry";
+
+
+/*
+==========================================================
+Bus Dispatcher
+==========================================================
+*/
+
+export {
+    BusDispatcher,
+} from "./busDispatcher";
+
+
+/*
+==========================================================
+Command Handler
+==========================================================
+*/
+
+export {
+    toCommandBusHandler,
+} from "./commandHandler";
+
+export type {
+    CommandHandler,
+} from "./commandHandler";
+
+
+/*
+==========================================================
+Command Registry
+==========================================================
+*/
+
+export {
+    CommandRegistry,
+} from "./commandRegistry";
+
+
+/*
+==========================================================
+Command Bus
+==========================================================
+*/
+
+export {
+    CommandBus,
+    commandBus,
+} from "./commandBus";
+
+
+/*
+==========================================================
+Event Handler
+==========================================================
+*/
+
+export {
+    toEventBusHandler,
+} from "./eventHandler";
+
+export type {
+    EventHandler,
+} from "./eventHandler";
+
+
+/*
+==========================================================
+Event Registry
+==========================================================
+*/
+
+export {
+    EventRegistry,
+} from "./eventRegistry";
+
+
+/*
+==========================================================
+Event Bus
+==========================================================
+*/
+
+export {
+    EventBus,
+    eventBus,
+} from "./eventBus";
+
+
+/*
+==========================================================
+Message Router
+==========================================================
+*/
+
+export {
+    MessageRouter,
+} from "./messageRouter";
+
+
+/*
+==========================================================
+Message Queue
+==========================================================
+*/
+
+export {
+    MessageQueue,
+} from "./messageQueue";
+
+
+/*
+==========================================================
+Message Processor
+==========================================================
+*/
+
+export {
+    MessageProcessor,
+} from "./messageProcessor";
