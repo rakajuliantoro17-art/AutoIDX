5a5409603dc840e19a00dc3dac13ff90a7a31297
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-08-10 22:31:15 +0700

    Refactor AI training service exports in index.ts

--- src/services/ai/training/index.ts
+++ src/services/ai/training/index.ts
@@ -1,1 +1,145 @@
+/**
+==========================================================
+AURA Trade OS
+AI Training Service
+Phase 33
+==========================================================
+*/
 
+/*
+ * Dataset
+ */
+
+export {
+    createDatasetSchema,
+    getFeature,
+} from "./dataset/datasetSchema";
+
+export type {
+    DatasetSchema,
+    DatasetValue,
+    DatasetFeatureType,
+    DatasetFeatureDefinition,
+} from "./dataset/datasetSchema";
+
+export {
+    createDatasetRecord,
+} from "./dataset/datasetRecord";
+
+export type {
+    DatasetRecord,
+    DatasetFeatures,
+} from "./dataset/datasetRecord";
+
+export {
+    splitDatasetChronologically,
+} from "./dataset/datasetSplit";
+
+export type {
+    DatasetSplit,
+    DatasetSplitConfig,
+} from "./dataset/datasetSplit";
+
+export {
+    DatasetBuilder,
+    createDatasetBuilder,
+} from "./dataset/datasetBuilder";
+
+export {
+    DatasetNormalizer,
+    datasetNormalizer,
+} from "./dataset/datasetNormalizer";
+
+export type {
+    NormalizationStatistics,
+    NormalizationResult,
+} from "./dataset/datasetNormalizer";
+
+export {
+    DatasetValidator,
+    datasetValidator,
+} from "./dataset/datasetValidator";
+
+export type {
+    DatasetValidationIssue,
+    DatasetValidationResult,
+} from "./dataset/datasetValidator";
+
+export {
+    DatasetManager,
+    datasetManager,
+} from "./dataset/datasetManager";
+
+export type {
+    TrainingDataset,
+} from "./dataset/datasetManager";
+
+/*
+ * Training
+ */
+
+export {
+    createTrainingConfig,
+} from "./trainingConfig";
+
+export type {
+    TrainingConfig,
+    TrainingAlgorithm,
+} from "./trainingConfig";
+
+export {
+    createTrainingRequest,
+} from "./trainingRequest";
+
+export type {
+    TrainingRequest,
+} from "./trainingRequest";
+
+export {
+    createTrainingProgress,
+} from "./trainingProgress";
+
+export type {
+    TrainingProgress,
+    TrainingStage,
+} from "./trainingProgress";
+
+export type {
+    TrainingMetricSet,
+    TrainingArtifact,
+    TrainingResult,
+} from "./trainingResult";
+
+export {
+    createTrainingJob,
+} from "./trainingJob";
+
+export type {
+    TrainingJob,
+    TrainingJobStatus,
+} from "./trainingJob";
+
+export {
+    DefaultTrainingEngine,
+} from "./trainingEngine";
+
+export type {
+    TrainingEngine,
+    TrainingEngineContext,
+} from "./trainingEngine";
+
+export {
+    TrainingManager,
+    trainingManager,
+} from "./trainingManager";
+
+export {
+    TrainingRegistry,
+    trainingRegistry,
+} from "./trainingRegistry";
+
+/*
+ * Pipeline
+ */
+
+export * from "./pipeline";
