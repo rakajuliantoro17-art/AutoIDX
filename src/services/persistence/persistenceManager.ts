90ed0f78a572d36d1c0f1b5801ab29e6e6d82425
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-08-11 20:56:40 +0700

    Add PersistenceManager interface and DefaultPersistenceManager class

--- src/services/persistence/persistenceManager.ts
+++ src/services/persistence/persistenceManager.ts
@@ -1,1 +1,37 @@
+import type {
+  ExecutionRepository,
+} from "./executionRepository";
 
+import type {
+  OrderRepository,
+} from "./orderRepository";
+
+import type {
+  PositionRepository,
+} from "./positionRepository";
+
+export interface PersistenceManager {
+  readonly executions:
+    ExecutionRepository;
+
+  readonly orders:
+    OrderRepository;
+
+  readonly positions:
+    PositionRepository;
+}
+
+export class DefaultPersistenceManager
+  implements PersistenceManager {
+
+  constructor(
+    public readonly executions:
+      ExecutionRepository,
+
+    public readonly orders:
+      OrderRepository,
+
+    public readonly positions:
+      PositionRepository,
+  ) {}
+}
