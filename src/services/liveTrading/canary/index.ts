129d8f221552b801b190a036931bc38cbfa26414
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-08-13 18:22:13 +0700

    Refactor exports in canary module index file

--- src/services/liveTrading/canary/index.ts
+++ src/services/liveTrading/canary/index.ts
@@ -4,21 +4,12 @@
  * Phase 38
  * ==========================================================
  */
-
 export * from "./canaryConfig";
-
 export * from "./canaryContext";
-
 export * from "./canaryDecision";
-
 export * from "./canaryGate";
-
 export * from "./canaryManager";
-
 export * from "./canaryExecutionService";
-
-export * from "./canaryExecutor.ts";
-
-export * from "./canaryGuard.ts";
-
-export * from "./canaryResult.ts";
+export * from "./canaryExecutor";
+export * from "./canaryGuard";
+export * from "./canaryResult";
